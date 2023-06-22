9a886cf0b717c020f876e389914c9b9117776630
Willian Andrade <willianandrade@MacBook-Pro-de-Willian.local>
2023-06-22 17:23:37 +0300

    first commit

---
+++ PROJETO/script.js
@@ -0,0 +1,25 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle("light")
+    
+    const img = document.querySelector("#profile img")
+
+    if(html.classList.contains("light")) {
+        img.setAttribute("src", "./assets/WillAvatar2.png")
+    } 
+
+    else {
+        img.setAttribute("src", "./assets/Will Avatar.png")
+    }
+    //if(html.classList.contains("light")) {
+      //      html.classList.remove("light")
+
+    //}
+
+    //else {
+
+    //    html.classList.add("light")
+    //}
+
+    
+}
